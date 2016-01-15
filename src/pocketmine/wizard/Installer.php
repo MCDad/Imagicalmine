de0d6bcfdd3a774e452603b598d4fb0b4d796cf7
keithkfng <keithkfng@gmail.com>
2016-01-15 02:45:35 +0100

    Revert "Added ClearSky changes"
    This reverts commit 651cf77ea6f448ca48761f9681c9877c9cc45cda.

--- src/pocketmine/wizard/Installer.php
+++ src/pocketmine/wizard/Installer.php
@@ -1,4 +1,29 @@
 <?php
+
+/*
+ *
+ *  _                       _           _ __  __ _             
+ * (_)                     (_)         | |  \/  (_)            
+ *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___  
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \ 
+ * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/ 
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___| 
+ *                     __/ |                                   
+ *                    |___/                                                                     
+ * 
+ * This program is a third party build by ImagicalMine.
+ * 
+ * ImagicalMine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ImagicalMine Team
+ * @link http://forums.imagicalcorp.ml/
+ * 
+ *
+*/
+
 /**
  * Set-up wizard used on the first run
  * Can be disabled with --no-wizard
@@ -18,7 +43,7 @@ class Installer{
 	private $lang;
 
 	public function __construct(){
-		echo "[*] ClearSky set-up wizard\n";
+		echo "[*] ImagicalMine set-up wizard\n";
 		echo "[*] Please select a language:\n";
 		foreach(InstallerLang::$languages as $short => $native){
 			echo " $native => $short\n";
@@ -65,7 +90,7 @@ class Installer{
   (at your option) any later version.
 
 LICENSE;
-		echo "\n[?] " . $this->lang->accept_license . " (y/N): ";
+		echo "\n[?] " . $this->lang->accept_license . " (Y/n): ";
 		if(strtolower($this->getInput("n")) != "y"){
 			echo "[!] " . $this->lang->you_have_to_accept_the_license . "\n";
 			sleep(5);
@@ -104,7 +129,13 @@ LICENSE;
 			echo "[?] " . $this->lang->default_gamemode . ": (" . self::DEFAULT_GAMEMODE . "): ";
 			$gamemode = (int) $this->getInput(self::DEFAULT_GAMEMODE);
 		}while($gamemode < 0 or $gamemode > 3);
-		$config->set("gamemode", $gamemode);
+		echo "[*] " . $this->lang->disable_logfile_info . "\n";
+		echo "[?] " . $this->lang->disable_logfile . " (Y/n): ";
+		if(strtolower($this->getInput("y")) == "n"){
+			$config->set("disable-logfile", 1);
+		}else{
+			$config->set("disable-logfile", 0);
+		}
 		echo "[?] " . $this->lang->max_players . " (" . self::DEFAULT_PLAYERS . "): ";
 		$config->set("max-players", (int) $this->getInput(self::DEFAULT_PLAYERS));
 		echo "[*] " . $this->lang->spawn_protection_info . "\n";
